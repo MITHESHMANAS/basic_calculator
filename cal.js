655bf5af121068f4e40d4fb3fd65044cf337ccd6
MITHESH <nmithesh06@gmail.com>
2025-12-19 14:18:58 +0530

    Fix formatting issues in cal.js

--- cal.js
+++ cal.js
@@ -2,13 +2,13 @@
 const displayy = document.getElementById("display");
 
 function setoperator(input) {
-    displayy.textContent += input;   // append to display
+    displayy.textContent += input;   
 }
 
 function cal() {
     const expr = displayy.textContent;
     try {
-        displayy.textContent = eval(expr);   // calculate
+        displayy.textContent = eval(expr);   
     } catch {
         displayy.textContent = "Error";
     }
